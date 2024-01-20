de670bc618dced02089f3d6aaccae9b1a6b31515
Laura <laurakwriting@gmail.com>
2024-01-20 16:27:08 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,5 +41,6 @@ html_theme_options = {
     'sticky_navigation': False,
     'navigation_depth': 3,
     'includehidden': True,
-    'titles_only': True
+    'titles_only': True,
+    'bodyfont': Georgia
 }
